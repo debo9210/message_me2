116eb425facdc8abe403b90ba365e225da918029
Omololu Oshoyemi <omololuadebowale1@gmail.com>
2019-02-05 07:26:00 +0000

    Update app folder

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,8 +1,11 @@
 class ChatroomController < ApplicationController
+  before_action :require_user
+  
   
   def index
     @root
+    @message = Message.new
     @messages = Message.all
-  end
+ end
   
 end
